8d33c6a1985cd8a7810aa430551cccdb4d75ee7e
chihaoyo <chihaoyo@gmail.com>
2020-06-19 12:10:08 +0800

    🚀 0.11 update model 1 en & tw

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ const config = {
   ],
   reports: [
     {
-      title: '中國對台資訊作戰事件研析，IO 研究室，2020/6/18。',
+      title: '中國對台資訊作戰事件研析，IO 研究室，2020/6/18，最後更新於 2020/6/19。',
       image: '/images/iorg-model-1-tw-cover.png',
       actions: [
         {
@@ -65,7 +65,7 @@ const config = {
       ]
     },
     {
-      title: 'Event Studies of Chinese Information Operations Against Taiwan, IORG, 2020/5/22, last updated 2020/6/18.',
+      title: 'Event Studies of Chinese Information Operations Against Taiwan, IORG, 2020/5/22, last updated 2020/6/19.',
       image: '/images/iorg-model-1-cover.png',
       actions: [
         {
