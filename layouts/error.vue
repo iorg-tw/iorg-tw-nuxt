eb3bbdf8b10b3cca2f79fea81838a2937dbc5cd2
chihaoyo <chihaoyo@gmail.com>
2020-06-23 14:21:15 +0800

    Add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="error">
+  <h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
+  <h1 v-else>404</h1>
+  <div>
+    <nuxt-link to="/">Go home</nuxt-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style lang="scss">
+.error {
+  margin: 1rem;
+}
+</style>
