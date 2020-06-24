028bc27631fe92e2f23f84d6ccdac54268b114cd
chihaoyo <chihaoyo@gmail.com>
2020-06-24 12:52:50 +0800

    Fine-tune Navbar layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,19 @@
 <template>
 <div class="navbar">
-  <a href="/">
+  <a class="go-home" href="/">
     <img src="/images/logo-tmp-small.png" width="64" />
   </a>
+  <div class="space"></div>
 </div>
 </template>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  > .go-home {
+    flex-grow: 0;
+    flex-shrink: 0;
+    width: 64px;
+  }
+}
+</style>
