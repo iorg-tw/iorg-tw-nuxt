cbf255cbfd97f86acf91a98e298b53d85552ed6a
chihaoyo <chihaoyo@gmail.com>
2020-07-09 11:32:38 +0800

    Use markdown syntax

--- data/config.js
+++ data/config.js
@@ -1,6 +1,6 @@
 const config = {
   title: 'IORG – Information Operations Research Group',
-  description: 'IORG is a Taiwan-based multidisciplinary research group focuses on information operations within the framework of cognitive warfare.',
+  description: 'IORG is a Taiwan-based multidisciplinary research group formed in 2019 by media workers, researchers, data scientists, and local activist. IORG researches *information manipulation* within the framework of *cognitive warfare*.',
   cover: '/images/cover-2-1.png'
 }
 
