bf6376979f3ed742e8404108fbb9e9a690270263
chihaoyo <chihaoyo@gmail.com>
2020-08-27 22:21:05 +0800

    Add i18n

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,45 @@
+<i18n lang="yaml">
+tw:
+  reports: "報告"
+  articles: "報導"
+  about: "關於"
+en:
+  reports: "Reports"
+  articles: "Articles"
+  about: "About"
+</i18n>
+
 <template>
 <div class="navbar">
   <nuxt-link class="go-home block" :to="{ path: '/' }">
     <img src="/images/logo-tmp-small.png" width="64" />
   </nuxt-link>
-  <div class="menu">
-    <nuxt-link :to="{ path: '/' }" class="item block"><span>Reports</span></nuxt-link>
-    <nuxt-link :to="{ name: 'a' }" class="item block"><span>Articles</span></nuxt-link>
-    <nuxt-link :to="{ path: '/about' }" class="item block"><span>About</span></nuxt-link>
+  <div class="menu main-menu">
+    <nuxt-link :to="{ path: '/' }" class="item block"><span>{{ $t('reports') }}</span></nuxt-link>
+    <nuxt-link :to="{ name: 'a' }" class="item block"><span>{{ $t('articles') }}</span></nuxt-link>
+    <nuxt-link :to="{ path: '/about' }" class="item block"><span>{{ $t('about') }}</span></nuxt-link>
+  </div>
+  <div class="menu langs">
+    <a href="#" v-for="locale of availableLocales" :key="locale.code" class="item block inverted minimal" @click.prevent.stop="$i18n.setLocale(locale.code)"><span>{{ locale.name }}</span></a>
   </div>
 </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 $size: 64px;
 .navbar {
   display: flex;
+  align-items: flex-start;
   > .go-home {
     flex-grow: 0;
     flex-shrink: 0;
@@ -22,16 +47,18 @@ $size: 64px;
   }
   > .menu {
     display: flex;
-    margin: 0 0.5rem;
+    padding: 0 0.5rem;
     > .item {
       display: block;
-      padding: 0 0.5rem;
+      padding: 0 0.375rem;
       line-height: $size;
-      color: var(--iorg-primary-dark-color);
-      > span {
-        line-height: 1.5;
-        border-bottom: 2px solid var(--iorg-primary-dark-color);
-      }
+    }
+  }
+  > .langs {
+    margin-left: auto;
+    background-color: var(--iorg-text);
+    > .item {
+      line-height: $size * 0.625;
     }
   }
 }
