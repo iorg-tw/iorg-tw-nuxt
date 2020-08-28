fab25ca3a83a17a0a4069dd626226c4d0ecc5f24
chihaoyo <chihaoyo@gmail.com>
2020-08-28 16:28:36 +0800

    Update IORG description

--- data/config.js
+++ data/config.js
@@ -1,6 +1,6 @@
 const config = {
   title: 'IORG – Information Operations Research Group',
-  description: 'IORG is a Taiwan-based multidisciplinary research group formed in 2019 by media workers, social scientists, data scientists, and local activist. IORG researches information manipulation within the framework of cognitive warfare.',
+  description: 'IORG is a Taiwan-based multidisciplinary research group formed in 2019 by media workers, social scientists, data scientists, and local activists. IORG researches information manipulation and strengthens information literacy in response to cognitive warfare.',
   cover: '/images/cover-2-1.png'
 }
 
