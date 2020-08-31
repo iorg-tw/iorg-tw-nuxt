19d7da4aa401e5038c5b9f794628d8a3e4009d33
chihaoyo <chihaoyo@gmail.com>
2020-08-31 20:49:08 +0800

    Add syncIONetwork & got

---
+++ tools/syncIONetwork.js
@@ -0,0 +1,22 @@
+const got = require('got')
+const fs = require('fs')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const apiKey = process.env.AIRTABLE_API_KEY
+
+async function get() {
+  try {
+    let nodes = await got(`https://api.airtable.com/v0/appu0MNR57XsXL8P6/nodes?api_key=${apiKey}`)
+    nodes = JSON.parse(nodes.body).records
+    fs.writeFileSync('data/ion/nodes.json', JSON.stringify(nodes, null, '\t'))
+
+    let edges = await got(`https://api.airtable.com/v0/appu0MNR57XsXL8P6/edges?api_key=${apiKey}`)
+    edges = JSON.parse(edges.body).records    
+    fs.writeFileSync('data/ion/edges.json', JSON.stringify(edges, null, '\t'))
+  } catch(error) {
+    console.log(error.response.body)
+  }
+}
+
+get()
