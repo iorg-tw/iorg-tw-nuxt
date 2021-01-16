f7b36cf53c549625741cc5308d17b6e420d36c93
chihaoyo <chihaoyo@gmail.com>
2021-01-16 10:18:39 +0800

    Write original airtable data; add dir & remove some categories

--- tools/syncIONetwork.js
+++ tools/syncIONetwork.js
@@ -16,42 +16,48 @@ const edgeCats = {
   ctrl: 'control',
   affl: 'affliation',
   info: 'info',
-  infoRev: 'infoRev',
-  infoBi: 'infoBi',
-  infoFC: 'infoFC'
+  fc: 'fc'
 }
 const edgeMatchCases = [
   {
-    exp: ['領導', '管理', '經營', '為母集團', '為所屬政黨', '為主導宮廟', '為上級單位', '擁有', '指導', '建立', '出版', '主管', '主持'],
-    cat: edgeCats.ctrl
+    exps: ['領導', '管理', '經營', '為母集團', '為所屬政黨', '為主導宮廟', '為上級單位', '擁有', '指導', '建立', '出版', '主管', '主持'],
+    cat: edgeCats.ctrl,
+    dir: 1
   },
   {
-    exactMatches: ['邀請前往中國交流', /(為|第\s?[0-9-]*\s?屆)主辦單位/, '相互推薦', '提出糾正案、獲通過', '合作', '參訪', '上節目'],
-    cat: edgeCats.affl
+    exps: ['邀請前往中國交流', /(為|第\s?[0-9-]*\s?屆)主辦單位/, '相互推薦', '提出糾正案、獲通過', '合作', '參訪', '上節目'],
+    cat: edgeCats.affl,
+    dir: 0
   },
   {
-    exactMatches: ['發起請願', '發表論文', '發文'],
-    cat: edgeCats.info
+    exps: ['發起請願', '發表論文', '發文'],
+    cat: edgeCats.info,
+    dir: 1
   },
   {
-    exactMatches: ['錯誤引用', '轉載', '改作', '引用', '專訪', '報導', '回應報導內容', '分享'],
-    cat: edgeCats.infoRev
+    exps: ['錯誤引用', '轉載', '改作', '引用', '專訪', '報導', '回應報導內容', '分享'],
+    cat: edgeCats.info,
+    dir: -1
   },
   {
-    exactMatches: ['貼文含相同關鍵詞', '發表相同內容'],
-    cat: edgeCats.infoBi
+    exps: ['貼文含相同關鍵詞', '發表相同內容'],
+    cat: edgeCats.info,
+    dir: 0
   },
   {
-    exactMatches: ['查核'],
-    cat: edgeCats.infoFC
+    exps: ['查核'],
+    cat: edgeCats.fc,
+    dir: -1
   },
   {
-    exp: [/^任/],
-    cat: edgeCats.ctrl
+    exps: [/^任/],
+    cat: edgeCats.ctrl,
+    dir: 1
   },
   {
-    exp: [/^為/, /^參加/],
-    cat: edgeCats.affl
+    exps: [/^為/, /^參加/],
+    cat: edgeCats.affl,
+    dir: 0
   }
 ]
 
@@ -59,7 +65,7 @@ const linkCats = {
   command: textMap.command
 }
 
-async function getList(listName) {
+async function fetchList(listName) {
   let list = []
   let offset = 0
   while(offset !== '' && offset !== null && offset !== undefined) {
@@ -72,16 +78,38 @@ async function getList(listName) {
   return list
 }
 
-async function get() {
+function readList(listName) {
+  let list = fs.readFileSync(`data/airtable/${listName}.json`, { encoding: 'utf8', flag: 'r' })
+  list = JSON.parse(list)
+  return list
+}
+
+function writeList(listName, list, dir = 'airtable') {
+  fs.writeFileSync(`data/${dir}/${listName}.json`, JSON.stringify(list, null, '\t'))
+}
+
+async function get(remote = false) {
   let allDomains
   let allNodes
   let allEdges
   let allLayouts
   try {
-    allDomains = await getList('domains')
-    allNodes = await getList('nodes')
-    allEdges = await getList('edges')
-    allLayouts = await getList('layouts')
+    if(remote) {
+      allDomains = await fetchList('domains')
+      allNodes = await fetchList('nodes')
+      allEdges = await fetchList('edges')
+      allLayouts = await fetchList('layouts')
+
+      writeList('domains', allDomains)
+      writeList('nodes', allNodes)
+      writeList('edges', allEdges)
+      writeList('layouts', allLayouts)
+    } else {
+      allDomains = readList('domains')
+      allNodes = readList('nodes')
+      allEdges = readList('edges')
+      allLayouts = readList('layouts')
+    }
   } catch(error) {
     console.error(error)
   }
@@ -119,9 +147,19 @@ async function get() {
     let action = d.fields.action
     let domains = d.fields.domains ? d.fields.domains : []
     let category = textMap.default
-    for(const cat of edgeCategories) {
-      if(cat.keywords.some(k => action.includes(k))) {
-        category = cat.id
+    let dir = 1
+    for(const matchCase of edgeMatchCases) {
+      if(matchCase.exps.some(exp => {
+        let result = false
+        if(exp.constructor.name === 'RegExp') {
+          result = action.match(exp)
+        } else if(exp.constructor.name === 'String') {
+          result = action === exp
+        }
+        return result
+      })) {
+        category = matchCase.cat
+        dir = matchCase.dir
       }
     }
     let notes = d.fields.notes ? d.fields.notes.trim() : null
@@ -131,6 +169,7 @@ async function get() {
       target: d.fields.to ? d.fields.to[0] : null,
       action,
       category,
+      dir,
       domains,
       notes
     }
@@ -177,10 +216,10 @@ async function get() {
   })
 
   try {
-    fs.writeFileSync('data/ion/domains.json', JSON.stringify(allDomains, null, '\t'))
-    fs.writeFileSync('data/ion/nodes.json', JSON.stringify(allNodes, null, '\t'))
-    fs.writeFileSync('data/ion/edges.json', JSON.stringify(allEdges, null, '\t'))
-    fs.writeFileSync('data/ion/layouts.json', JSON.stringify(allLayouts, null, '\t'))
+    writeList('domains', allDomains, 'ion')
+    writeList('nodes', allNodes, 'ion')
+    writeList('edges', allEdges, 'ion')
+    writeList('layouts', allLayouts, 'ion')
   } catch(error) {
     console.error(error)
   }
