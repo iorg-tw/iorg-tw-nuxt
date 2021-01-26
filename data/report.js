11ea23eb6746fa63073ec10a07d45bbc297e5c1c
chihaoyo <chihaoyo@gmail.com>
2021-01-27 02:41:25 +0800

    Add B & D & Acknowledgement

--- data/report.js
+++ data/report.js
@@ -1,45 +1,56 @@
 const articleMap = {
   keyFindings: {
     publicURL: 'https://docs.google.com/document/d/e/2PACX-1vTP2pZvsbfGbERQCtaIduiRkgsRduLp68wBPZOpM6M9mk2ruSX5qrHGbsN3ira567Dc6qj65V0vpqTG/pub'
+  },
+  acknowledgement: {
+    publicURL: 'https://docs.google.com/document/d/e/2PACX-1vTbqKB1uun54pSxk6lD-I_NhpGxyYFEc2jsw-KPT-fY9JVNk_NsveMJwVGpYqIC5GcSerugWMhDOrfC/pub'
   }
 }
 
-const sections = [
+const topics = [
   {
     id: 'A',
-    title: '中國對台滲透網絡圖'
+    title: '中國對台滲透網絡圖',
+    to: '/r/a'
   },
   {
     id: 'B',
-    title: '資訊操弄'
+    title: '資訊操弄',
+    to: '/r/b'
   },
   {
     id: 'D',
-    title: '人際滲透'
+    title: '人際滲透',
+    to: '/r/d'
   },
   {
     id: 'P1',
-    title: '事實查核有效性'
+    title: '事實查核有效性',
+    to: '/r/p/1'
   },
   {
     id: 'P2',
-    title: '受操弄資訊內容演化'
+    title: '受操弄資訊內容演化',
+    to: '/r/p/2'
   },
   {
     id: 'W',
-    title: '2020 年度全國工作坊成果'
+    title: '2020 年度全國在地連結成果報告',
+    to: '/community-engagement-2020'
   },
   {
     id: 'F',
-    title: '未來研究'
+    title: '未來研究',
+    to: '/roadmap'
   },
   {
     id: 'G',
-    title: '名詞定義'
+    title: '名詞定義',
+    to: '/glossary'
   }
 ]
 
 module.exports = {
   articleMap,
-  sections
+  topics
 }
