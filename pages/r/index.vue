9225c5b9b4a5b2a44cde766aa194b020617996cd
chihaoyo <chihaoyo@gmail.com>
2021-01-27 02:40:47 +0800

    Update report K

--- pages/r/index.vue
+++ pages/r/index.vue
@@ -1,25 +1,52 @@
+<i18n lang="yaml">
+tw:
+  reportTitle: "中國對台滲透研究報告"
+  keyFindings: "研究總結"
+en:
+  reportTitle: "Report on Chinese Infiltration Against Taiwan"
+  keyFindings: "Key Findings"
+</i18n>
+
 <template>
 <div class="page">
-  <div class="key-findings">
-    <h2>Key findings</h2>
-    <div class="findings" v-html="docK.html"></div>
+  <div class="container">
+    <div class="panel text">
+      <h1>{{ $t('reportTitle') }}</h1>
+      <h2>{{ $t('keyFindings') }}</h2>
+    </div>
   </div>
-  <div class="sections">
-    <div v-for="section of sections" :key="section.id" class="section">{{ section.id }} {{ section.title }}</div>
+  <div class="findings container">
+    <div v-for="(finding, findingIndex) of findings" :key="findingIndex" class="finding panel filled tiled">
+      <div class="header detail" v-html="finding.header"></div>
+      <div class="body detail" v-html="finding.body"></div>
+    </div>
+  </div>
+  <div class="topics container">
+    <div v-for="topic of topics" :key="topic.id" class="topic panel tiled compact">
+      <nuxt-link :to="topic.to">{{ topic.id }} {{ topic.title }}</nuxt-link>
+    </div>
+  </div>
+  <div class="acknowledgement container">
+    <div class="content panel text" v-html="docAck.html"></div>
   </div>
 </div>
 </template>
 
 <script>
-import { articleMap, sections } from '~/data/report'
-import { getDoc } from '~/lib/gdoc'
+import { articleMap, topics } from '~/data/report'
+import { getDoc, toSectionArray } from '~/lib/gdoc'
 
 export default {
   async asyncData() {
     const docK = await getDoc(articleMap.keyFindings.publicURL)
+    const findings = toSectionArray(docK.html, 'h3')
+
+    const docAck = await getDoc(articleMap.acknowledgement.publicURL)
+
     return {
-      docK,
-      sections
+      findings,
+      topics,
+      docAck
     }
   }
 }
@@ -27,23 +54,12 @@ export default {
 
 <style lang="scss">
 .page {
-  > .key-findings {
-    > .findings {
-      > div {
-        display: flex;
-        flex-wrap: wrap;
-        p {
-          width: 50%;
-          font-size: 1.25rem;
-        }
-      }
+  > .findings {
+    > .finding {
     }
   }
-  > .sections {
-    display: flex;
-    flex-wrap: wrap;
-    > .section {
-      width: 25%;
+  > .topics {
+    > .topic {
     }
   }
 }
