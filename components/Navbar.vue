683c90c1eaeef901ed363b2c61e2ec20938e06b9
chihaoyo <chihaoyo@gmail.com>
2021-02-10 00:23:59 +0800

    Assume TW & EN only; use switchLocalePath

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -23,7 +23,7 @@ _en:
     <nuxt-link :to="localePath('/e')" class="item block"><span>{{ $t('events') }}</span></nuxt-link>
   </div>
   <div class="menu langs">
-    <a v-for="locale of availableLocales" :key="locale.code" href="#" class="item block minimal" @click.prevent.stop="$i18n.setLocale(locale.code)"><span>{{ locale.name }}</span></a>
+    <nuxt-link v-for="locale of availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)" class="item block minimal"><span>文↔︎A</span></nuxt-link><!-- FIXME: assumes only two langs TW & EN -->
   </div>
 </div>
 </template>
@@ -44,6 +44,7 @@ export default {
 .navbar {
   $size: 64px;
   display: flex;
+  flex-wrap: nowrap;
   align-items: flex-start;
   > .go-home {
     flex-grow: 0;
@@ -55,6 +56,7 @@ export default {
     padding: 0 0.5rem;
     > .item {
       display: block;
+      margin: 0;
       padding: 0 0.375rem;
       line-height: $size;
     }
@@ -62,24 +64,33 @@ export default {
   > .main-menu {
     > .item {
       @media (max-width: 400px) {
-        padding: 0 0.125rem;
+        padding: 0;
         word-break: break-all;
         min-width: 2.5rem;
         line-height: 1.15;
-        transform: skew(0, -12deg) translateY(0.75rem) scale(0.875);
+        transform: skew(0, -12deg) translateY(1rem) scale(0.875);
         transform-origin: bottom left;
+      }
+      &:first-child {
+        padding-left: 0;
+      }
+      &:last-child {
+        padding-right: 0;
       }
     }
   }
   > .langs {
     margin-left: auto;
-    margin-top: 0.5rem;
-    margin-right: 0.5rem;
-    background-color: var(--iorg-accent);
-    border-radius: $size * 0.625 / 2;
-    @include shadow;
+    padding: 0.5rem 0.5rem 0 0;
     > .item {
+      padding: 0 0.75rem;
       line-height: $size * 0.625;
+      background-color: var(--iorg-accent);
+      border-radius: $size * 0.625 / 2;
+      @include shadow;
+      > span {
+        white-space: nowrap;
+      }
     }
   }
 }
