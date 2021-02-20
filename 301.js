c09e2f66aa41852cff2373faf6f35fe9626b318e
chihaoyo <chihaoyo@gmail.com>
2021-02-21 00:19:06 +0800

    Update 301 rules to redirect /f/r-{s} → /r/{s}

--- 301.js
+++ 301.js
@@ -2,5 +2,6 @@ module.exports = [
   { from: '/events', to: '/e' },
   { from: '/v', to: '/a' },
   { from: '/i/hong-sh', to: '/a/hong-sh' },
-  { from: '/f/r/covid-19', to: '/r/covid-19' }
+  { from: '/f/r-covid-19', to: '/r/covid-19' },
+  { from: '/f/r-', to: '/r/' }
 ]
