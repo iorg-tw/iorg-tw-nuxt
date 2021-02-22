753fbabe9cc5c477351e034f982b286a55c3df06
chihaoyo <chihaoyo@gmail.com>
2021-02-22 11:04:56 +0800

    Update custom events

--- data/custom-events.js
+++ data/custom-events.js
@@ -2,15 +2,15 @@ const customEvents = [
   {
     id: '305',
     localizedDocs: {
-      _tw: 'https://docs.google.com/document/d/e/2PACX-1vRwnDPBVwOyZQ3sWwgtvmnImWjjco1ynJvMdVr0rY5TxbuUmoaFCqQ00ItV-9NKEjlnAyBw901uISFX/pub',
-      _en: 'https://docs.google.com/document/d/e/2PACX-1vQhtioJ53vJGtMK6QQr4Q_6gCOiy9bpn2IjOpa_WPKGJXblb1aBQ7E5YqbsFb7X6f6Jdc7yVb0DxjQN/pub'
+      _tw: 'https://docs.google.com/document/d/e/2PACX-1vR9SlDEg0hN0vJx2LHAW06sDbsNGP3oLaq5t3tuazY1kgbltjOCFfVfyViCoGGR9LCM8gdLOSkCP-zY/pub',
+      _en: 'https://docs.google.com/document/d/e/2PACX-1vSEzqaTYk9HK_wjnP4f7QWk0DMIOlWUf-gtkq-sn5AlEVmPuDyHeXMy04VW5mjFTEz3swrLt7Zqy63l/pub'
     }
   },
   {
     id: '306',
     localizedDocs: {
-      _tw: 'https://docs.google.com/document/d/e/2PACX-1vSUXXi3dQlyhdfRwN6ZuZcHY693UIvsx9158faMs5wghbXTTvhihW3N3_3bD6nPx0AlCAXd7z1veVz4/pub',
-      _en: 'https://docs.google.com/document/d/e/2PACX-1vTLpbZ4WIeaczNZX0diEIctA4H8_TFry5rGTrmZi8F39zZIODDWCCCDfiTeYQc6tj1ML-B9swDkFF0R/pub'
+      _tw: 'https://docs.google.com/document/d/e/2PACX-1vSZO665xPuxsc11-ADDzOm8srJ7ZTDjAP7Fhl3rLR4hQX8GWNez8qGu6KQbD4CRTAG7PLTJ9iL4nMR7/pub',
+      _en: 'https://docs.google.com/document/d/e/2PACX-1vRQRDnBvfsxNN2G9FTH3x_jns5d8kAn39ckejuWHPgnaKKBdjrNE3tLRZqPFcM_puhLOicnGqqzAd-6/pub'
     }
   }
 ]
