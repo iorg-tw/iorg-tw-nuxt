0673995a5423cd38ecb16d11925e66f9ead154b1
chihaoyo <chihaoyo@gmail.com>
2021-02-25 17:56:56 +0800

    Try /r/d

--- pages/r/d.vue
+++ pages/r/d.vue
@@ -1,14 +1,18 @@
 <template>
 <div class="page research">
   <div class="section-header">
-    <h1>{{ CONST.code }} {{ $t(CONST.id) }}</h1>
+    <h1>{{ doc.title }}</h1>
   </div>
+  <google-doc :doc="doc" :options="{ metaphor: 'page', head: false }" class="doc-d" />
   <node-list :nodes="nodes" />
 </div>
 </template>
 
 <script>
 import tree from '~/data/research-tree'
+import { getDoc } from '~/lib/gdoc'
+import { generateMeta } from '~/lib/meta'
+import GoogleDoc from '~/components/GoogleDoc'
 import NodeList from '~/components/NodeList'
 
 const CONST = {
@@ -18,7 +22,15 @@ const CONST = {
 
 export default {
   components: {
+    GoogleDoc,
     NodeList
+  },
+  async asyncData() {
+    const url = 'https://docs.google.com/document/d/e/2PACX-1vRpuzArr1SgHaSd-sErL_0dM2Fj4fESvM8WMplzhz9nQPaTI57bf7nM4lNWbDvsVkdrKuwXHbn87s2t/pub'
+    const doc = await getDoc(url)
+    return {
+      doc
+    }
   },
   data() {
     const nodes = tree.filter(node => node.parentID === CONST.id)
@@ -26,6 +38,9 @@ export default {
       nodes,
       CONST
     }
+  },
+  head() {
+    return generateMeta(this.doc.title)
   }
 }
 </script>
