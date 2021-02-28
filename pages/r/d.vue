dab51305da4f2db539ffe3a17919bc304ae5dd2e
chihaoyo <chihaoyo@gmail.com>
2021-02-28 16:32:53 +0800

    Update /r/d

--- pages/r/d.vue
+++ pages/r/d.vue
@@ -3,14 +3,21 @@
   <div class="section-header">
     <h1>{{ doc.title }}</h1>
   </div>
-  <google-doc :doc="doc" :options="{ metaphor: 'page', head: false }" class="doc-d" />
+  <div v-for="objL0 of structuredDoc" :key="objL0.title" class="group">
+    <google-doc :doc="objL0" :options="{ metaphor: 'page' }" />
+    <div class="findings">
+      <div v-for="objL1 of objL0.children" :key="objL1.title" class="finding">
+        <google-doc :doc="objL1" :options="{ metaphor: 'page', enableToggle: true }" />
+      </div>
+    </div>
+  </div>
   <node-list :nodes="nodes" />
 </div>
 </template>
 
 <script>
 import tree from '~/data/research-tree'
-import { getDoc } from '~/lib/gdoc'
+import { getDoc, structureDoc } from '~/lib/gdoc'
 import { generateMeta } from '~/lib/meta'
 import GoogleDoc from '~/components/GoogleDoc'
 import NodeList from '~/components/NodeList'
@@ -28,8 +35,10 @@ export default {
   async asyncData() {
     const url = 'https://docs.google.com/document/d/e/2PACX-1vRpuzArr1SgHaSd-sErL_0dM2Fj4fESvM8WMplzhz9nQPaTI57bf7nM4lNWbDvsVkdrKuwXHbn87s2t/pub'
     const doc = await getDoc(url)
+    const structuredDoc = structureDoc(doc.html, ['h2', 'h3'])
     return {
-      doc
+      doc,
+      structuredDoc
     }
   },
   data() {
@@ -50,5 +59,13 @@ export default {
 
 .page.research {
   padding: $default-page-padding;
+  > .section-header {
+    margin-bottom: 0.5rem;
+  }
+  > .group {
+    > .findings {
+      margin-bottom: 1.5rem;
+    }
+  }
 }
 </style>
