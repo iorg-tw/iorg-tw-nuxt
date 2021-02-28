6f770a6ac959bdd641c1e96ddb8a46eb2989a5f4
chihaoyo <chihaoyo@gmail.com>
2021-02-28 17:26:00 +0800

    Update doc demo

--- pages/demo/doc.vue
+++ pages/demo/doc.vue
@@ -1,20 +1,24 @@
 <template>
 <div class="page demo">
-  <div v-for="objL0 of structuredDoc" :key="objL0.header" class="objL0">
-    <div :is="objL0.titleTag">{{ objL0.title }}</div>
-    <div class="html" v-html="objL0.html"></div>
-    <div v-if="objL0.children" class="children">
-      <div v-for="objL1 of objL0.children" :key="objL1.header" class="objL1">
-        <div :is="objL1.titleTag">{{ objL1.title }}</div>
-        <div class="html" v-html="objL1.html"></div>
+  <div class="demo demo-1">
+    <a class="demo-header block"><span>Demo 1</span></a>
+    <div v-for="objL0 of structuredDoc" :key="objL0.header" class="objL0">
+      <div :is="objL0.titleTag">{{ objL0.title }}</div>
+      <div class="html" v-html="objL0.html"></div>
+      <div v-if="objL0.children" class="children">
+        <div v-for="objL1 of objL0.children" :key="objL1.header" class="objL1">
+          <div :is="objL1.titleTag">{{ objL1.title }}</div>
+          <div class="html" v-html="objL1.html"></div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="separator"></div>
-  <div class="demo-2">
+  <div class="demo demo-2">
+    <a class="demo-header block"><span>Demo 2</span></a>
     <google-doc :doc="doc" />
   </div>
-  <div class="demo-3">
+  <div class="demo demo-3">
+    <a class="demo-header block"><span>Demo 3</span></a>
     <google-doc :doc="doc" :options="{ metaphor: 'page' }" />
   </div>
 </div>
@@ -22,6 +26,7 @@
 
 <script>
 import { getDoc, structureDoc } from '~/lib/gdoc'
+import { generateMeta } from '~/lib/meta'
 import GoogleDoc from '~/components/GoogleDoc'
 
 export default {
@@ -37,20 +42,30 @@ export default {
       doc,
       structuredDoc
     }
+  },
+  head() {
+    return generateMeta(this.doc.title, this.doc.subtitle, this.doc.summary, this.doc.coverImage)
   }
 }
 </script>
 
 <style lang="scss">
 .page.demo {
-  .objL0 {
-    margin: 1rem;
-    border: 2px solid black;
-    padding: 0.75rem;
-    .objL1 {
+  > .demo {
+    margin-bottom: 4rem;
+    > .demo-header {
       margin: 1rem;
-      border: 1px solid black;
+      font-weight: bold;
+    }
+    .objL0 {
+      margin: 1rem;
+      border: 2px solid black;
       padding: 0.75rem;
+      .objL1 {
+        margin: 1rem;
+        border: 1px solid black;
+        padding: 0.75rem;
+      }
     }
   }
 }
