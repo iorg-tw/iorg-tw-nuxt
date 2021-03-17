9b0b0ae5824c3d44b208e0d1451fd02b1028a074
chihaoyo <chihaoyo@gmail.com>
2021-03-18 01:59:35 +0800

    Update roadmap

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -1,5 +1,35 @@
 <template>
-<div class="page">
-  <h1>Roadmap</h1>
+<div class="page roadmap">
+  <google-doc :doc="doc" :options="{ showSummary: false }" />
 </div>
 </template>
+
+<script>
+import articleMap from '~/data/articles'
+import { getDoc } from '~/lib/gdoc'
+import { generateMeta } from '~/lib/meta'
+import GoogleDoc from '~/components/GoogleDoc'
+
+export default {
+  components: {
+    GoogleDoc
+  },
+  async asyncData() {
+    const doc = await getDoc(articleMap._F.publicURLs._tw)
+    return {
+      doc
+    }
+  },
+  head() {
+    return generateMeta(this.doc.title)
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/styles/resources';
+
+.page.open {
+  padding: $default-page-padding;
+}
+</style>
