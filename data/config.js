9fa3cf7ce4059e6241cf546ba2e20d4aa9a6547a
chihaoyo <chihaoyo@gmail.com>
2021-04-09 20:34:34 +0800

    Use fully-qualified HTTPS path for meta image

--- data/config.js
+++ data/config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'IORG – Information Operations Research Group',
   description: 'IORG is a Taiwan-based multidisciplinary research group formed in 2019 by media workers, social scientists, data scientists, and local activists. IORG researches information manipulation and strengthens information literacy in response to cognitive warfare.',
-  cover: '/images/cover-2-1.png'
+  cover: 'https://iorg.tw/images/cover-2-1.png'
 }
 
 module.exports = config
