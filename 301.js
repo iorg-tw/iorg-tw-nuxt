c220b5d456c009329262bfe37883411419e7e3eb
chihaoyo <chihaoyo@gmail.com>
2021-05-27 07:37:54 +0800

    Use 301 for short urls (maybe a bad idea)

--- 301.js
+++ 301.js
@@ -4,5 +4,7 @@ module.exports = [
   { from: '/v', to: '/a' },
   { from: '/i', to: '/a' },
   { from: /^\/[iv]{1}\/(.+)$/, to: '/a/$1' },
-  { from: /^\/f\/r-(.+)$/, to: '/r/$1' }
+  { from: /^\/f\/r-(.+)$/, to: '/r/$1' },
+  // short urls
+  { from: '/f/print', to: 'https://drive.google.com/drive/folders/1rhS-SA6rCELnS9XtzByPAHHIe7ymHlEe' }
 ]
