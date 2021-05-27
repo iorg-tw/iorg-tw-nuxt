e21f759e852afe6bd1167f7ba853a19a0bc7f13f
chihaoyo <chihaoyo@gmail.com>
2021-05-27 13:03:01 +0800

    Test more short urls

--- 301.js
+++ 301.js
@@ -6,5 +6,7 @@ module.exports = [
   { from: /^\/[iv]{1}\/(.+)$/, to: '/a/$1' },
   { from: /^\/f\/r-(.+)$/, to: '/r/$1' },
   // short urls
+  { from: '/f/co', to: 'https://docs.google.com/document/d/1jzXzNCLb5NzY8-ZpnKu548NtR3xAsiisGszA-foiFNs/edit'},
+  { from: '/f/sns', to: 'https://docs.google.com/document/d/14LXxlD_hPicVcixhXermPXtaVjso8vASgqRqlhjuui0/edit'},
   { from: '/f/print', to: 'https://drive.google.com/drive/folders/1rhS-SA6rCELnS9XtzByPAHHIe7ymHlEe' }
 ]
