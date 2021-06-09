bb31ffab516cd33f990fcde6dc9f03e95c7beda8
chihaoyo <chihaoyo@gmail.com>
2021-06-09 16:19:31 +0800

    Add page to subscribe to D.A.

---
+++ pages/da/sub.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="page subscribe">
+  <subscribe-simple />
+</div>
+</template>
+
+<script>
+import { generateMeta } from '~/lib/meta'
+import SubscribeSimple from '~/components/SubscribeSimple'
+
+export default {
+  components: {
+    SubscribeSimple
+  },
+  head() {
+    return generateMeta('訂閱心慌週報')
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/styles/resources';
+
+.page.subscribe {
+  > .subscribe-simple {
+    margin: 0 auto;
+  }
+}
+</style>
