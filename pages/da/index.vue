97543ae36189d300ac8818d2282bf7debffca9a8
chihaoyo <chihaoyo@gmail.com>
2021-07-20 08:32:39 +0800

    Add dokidoki listing

--- pages/da/index.vue
+++ pages/da/index.vue
@@ -15,16 +15,28 @@ _en:
     <p class="section-title-fancy">{{ $t('articles') }}</p>
   </div>
   <article-list type="da" />
+  <google-doc :doc="doc" :options="{ metaphor: 'page', head: false }" class="doc-doki-list" />
 </div>
 </template>
 
 <script>
 import { generateMeta } from '~/lib/meta'
+import { getLocalizedArticles } from '~/lib/i18n'
 import ArticleList from '~/components/ArticleList'
+import GoogleDoc from '~/components/GoogleDoc'
+
+const DOKI_LIST = 'dokidoki-listing'
 
 export default {
   components: {
-    ArticleList
+    ArticleList,
+    GoogleDoc
+  },
+  async asyncData({ app }) {
+    const [doc] = await getLocalizedArticles([DOKI_LIST], app.i18n.locale, app.i18n.defaultLocale)
+    return {
+      doc
+    }
   },
   head() {
     return generateMeta(this.$t('title'), null, this.$t('description'))
