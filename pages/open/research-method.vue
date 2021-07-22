fbb17022f018b60492e28efe63b97a5fb5523d4f
chihaoyo <chihaoyo@gmail.com>
2021-07-22 21:51:08 +0800

    Clean-up & restructure

---
+++ pages/open/research-method.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="page article sys">
+  <google-doc :doc="doc" />
+</div>
+</template>
+
+<script>
+import { getLocalizedArticles } from '~/lib/i18n'
+import { generateMeta } from '~/lib/meta'
+import GoogleDoc from '~/components/GoogleDoc'
+
+export default {
+  components: {
+    GoogleDoc
+  },
+  async asyncData({ app, params, error }) {
+    const id = '_O_RM'
+    const [doc] = await getLocalizedArticles([id], app.i18n.locale, app.i18n.defaultLocale)
+    if(!doc) {
+      error({ statusCode: 404, message: 'pageNotFound' })
+      return
+    }
+    return {
+      doc
+    }
+  },
+  head() {
+    return generateMeta(this.doc.title)
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/styles/resources';
+
+.page.article.sys {
+  padding: $default-page-padding;
+}
+</style>
