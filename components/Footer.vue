f1294572c11f51d1ea918ae4961b6c330345ecec
chihaoyo <chihaoyo@gmail.com>
2023-06-03 08:09:43 +0800

    Minor update

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
 _tw:
   about: "關於 IORG"
   open: "預設開放"
-  research: "研究成果"
+  research: "研究"
   articles: "報導"
   events: "活動"
   email: "電子郵件"
@@ -31,7 +31,7 @@ _en:
       <p><span class="emoji">📍</span> <nuxt-link :to="localePath('/e')">{{ $t('events') }}</nuxt-link></p>
     </div>
     <div class="panel">
-      <p><span class="emoji">📬</span> {{ $t('email') }} <a href="#">hi [at] iorg.tw</a></p>
+      <p><span class="emoji">📬</span> {{ $t('email') }} <a href="#">hi [at] iorg [.] tw</a></p>
       <p><span class="emoji">🐦</span> <a href="https://twitter.com/iorgtw" target="_blank">@iorgtw</a></p>
       <p><span class="emoji">📙</span> <a href="https://instagram.com/infolitguide" target="_blank">資訊判讀力</a></p>
       <p><span class="emoji">🫤</span> <a href="https://facebook.com/iorgtw" target="_blank">IORG</a></p>
