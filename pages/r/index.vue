179f666d7663dc631291d1fe349b602289f5cf0f
chihao <chihaoyo@gmail.com>
2024-01-30 11:58:01 +0800

    Update /r

--- pages/r/index.vue
+++ pages/r/index.vue
@@ -16,6 +16,24 @@ _en:
 <template>
 <div class="page research-home">
   <div class="page-header section-header">
+    <h1>{{ $t('r_title_2023') }}</h1>
+    <p class="subtitle">{{ $t('r_subtitle_2023') }}</p>
+  </div>
+  <div class="key-findings">
+    <div v-for="objL0 of structuredDocK2023" :key="objL0.title" class="group">
+      <div class="group-header section-header">
+        <div :is="objL0.titleTag" class="group-title">{{ objL0.title }}</div>
+        <nuxt-link :to="localePath('/r/2023')" class="more button small">{{ $t('more') }}</nuxt-link>
+      </div>
+      <div v-if="objL0.children" class="findings container">
+        <div v-for="objL1 of objL0.children" :key="objL1.title" class="finding panel tiled large filled raised">
+          <p class="header detail">{{ objL1.title }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="divider"></div>
+  <div class="page-header section-header">
     <h1>{{ $t('r_title_2022') }}</h1>
     <p class="subtitle">{{ $t('r_subtitle_2022') }}</p>
   </div>
@@ -95,7 +113,7 @@ export default {
     Intro
   },
   async asyncData({ app }) {
-    const [docK2020, docK2021, docK2022, docAck] = await getLocalizedArticles(['_R_2020', '_R_2021', '_R_2022', '_ack'], app.i18n.locale)
+    const [docK2020, docK2021, docK2022, docK2023, docAck] = await getLocalizedArticles(['_R_2020', '_R_2021', '_R_2022', '_R_2023', '_ack'], app.i18n.locale)
     return {
       structuredDocK2020: structureDoc(docK2020.html, ['h2', 'h3']),
       level0Nodes2020: tree.filter(node => node.group === 2020 && node.level === 0),
@@ -103,6 +121,8 @@ export default {
       level0Nodes2021: tree.filter(node => node.group === 2021 && node.level === 0),
       structuredDocK2022: structureDoc(docK2022.html, ['h2', 'h3']),
       level0Nodes2022: tree.filter(node => node.group === 2022 && node.level === 0),
+      structuredDocK2023: structureDoc(docK2023.html, ['h2', 'h3']),
+      level0Nodes2023: tree.filter(node => node.group === 2023 && node.level === 0),
       level0NodesSys: tree.filter(node => node.group === 0 && node.level === 0),
       docAck
     }
