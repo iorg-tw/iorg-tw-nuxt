fc4f0a33e66039b1b7ab8f1d4d16a7f29ebf96ed
chihaoyo <chihaoyo@gmail.com>
2024-07-06 19:55:28 +0800

    Add filter to ArticleList

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -3,20 +3,25 @@ _tw:
   publishedAt: "發布"
   updatedAt: "更新"
   more: "更多⋯"
+  all: "全部"
 _en:
   publishedAt: "Published"
   updatedAt: "Updated"
   more: "More…"
+  all: "All"
 </i18n>
 
 <template>
 <div class="article-list container-wrapper">
+  <div v-if="showFilter" class="filter">
+    <a class="button small" :class="{ 'active': !filterIsActive }" @click="resetFilter">{{ $t('all') }}</a><a v-for="(selected, tag) in tagSelection" :key="tag" class="button small" :class="{ 'active': selected }" @click="toggleTag(tag)">{{ $t(`tag_${tag}`) }}</a>
+  </div>
   <div class="articles container">
-    <template v-for="(panel, panelIndex) of visiblePanels">
-      <div v-if="['links'].includes(panel.type)" :key="panelIndex" class="panel tiled xlarge">
+    <template v-for="(panel, panelIndex) of panelsInRange">
+      <div v-if="['links'].includes(panel.type) && panel.show" :key="panelIndex" class="panel tiled xlarge">
         <link-list group="home" />
       </div>
-      <nuxt-link v-if="['article', 'featured'].includes(panel.type)" :key="panelIndex" :to="localeRoute(panel.data.path ? { path: panel.data.path } : { name: 'a-id', params: { id: panel.data.id } })" class="article block panel tiled filled" :class="articleClasses">
+      <nuxt-link v-if="['article', 'featured'].includes(panel.type) && panel.show" :key="panelIndex" :to="localeRoute(panel.data.path ? { path: panel.data.path } : { name: 'a-id', params: { id: panel.data.id } })" class="article block panel tiled filled" :class="articleClasses">
         <img v-if="getLocalizedDoc(panel.data).coverImage" :src="getLocalizedDoc(panel.data).coverImage" class="cover" />
         <div class="detail">
           <h3 v-html="optimizeTracking(getLocalizedDoc(panel.data).title)"></h3>
@@ -27,12 +32,12 @@ _en:
           </div>
         </div>
       </nuxt-link>
-      <div v-if="['sub'].includes(panel.type)" :key="panelIndex" class="panel tiled">
+      <div v-if="['sub'].includes(panel.type) && panel.show" :key="panelIndex" class="panel tiled">
         <subscribe-simple />
       </div>
     </template>
-    <div v-if="visiblePanels.length < panels.length" class="panel tiled" key="more">
-      <a class="button action average" @click="currentListSize += pageSize">{{ $t('more') }}</a>
+    <div v-if="panelsInRange.length < panelsToShow.length" key="more" class="panel tiled">
+      <a class="button action average" @click="currentListRange += pageSize">{{ $t('more') }}</a>
     </div>
   </div>
 </div>
@@ -73,61 +78,98 @@ export default {
     const showFeaturedArticles = this.features.includes('show-featured-articles')
     const showAll = this.features.includes('show-all')
     const showSub = this.features.includes('show-sub')
-    const isDAList = this.type === 'da'
+    const showFilter = this.features.includes('show-filter')
+    const articleClasses = []
+    if(this.type === 'da') {
+      articleClasses.push('xlarge')
+    }
 
     let computedType = this.type
     if(showAll) {
       computedType = 'article da eval video research sys'
     }
     const keys = Object.keys(allArticles)
-    const articles = keys.filter(k => this.showAll ? true : allArticles[k].show && computedType.includes(allArticles[k].type)).map(k => allArticles[k])
+    const articles = keys.filter(k => allArticles[k].show && computedType.includes(allArticles[k].type)).map(k => allArticles[k])
     const featuredArticles = keys.filter(k => allArticles[k].featured).map(k => allArticles[k])
 
     const panels = []
     if(showHomeLinks) {
-      panels.push({ type: 'links' })
+      panels.push({ type: 'links', show: true })
     }
     if(showFeaturedArticles) {
       panels.push(...featuredArticles.map(a => ({ type: 'featured', data: a })))
     }
     if(articles.length > 0) {
-      panels.push({ type: 'article', data: articles[0] })
+      panels.push({ type: 'article', data: articles[0], show: true })
       if(showSub) {
         panels.push({ type: 'sub' })
       }
       for(let i = 1; i < articles.length; i++) {
-        panels.push({ type: 'article', data: articles[i] })
+        panels.push({ type: 'article', data: articles[i], show: true })
       }
     }
 
-    const currentListSize = this.firstPageSize
+    const tagSelection = Object.assign({}, ...articles
+      .map(a => a.tags ? a.tags.split(' ').filter(t => t.trim().length > 0) : []) // extract tags
+      .reduce((a, v) => a.concat(v), []) // flatten array
+      .filter((v, i, a) => a.indexOf(v) === i) // unique
+      .map(t => ({ [t]: false }))
+    )
+
+    const currentListRange = this.firstPageSize
 
     return {
       showHomeLinks,
       showFeaturedArticles,
       showAll,
       showSub,
+      showFilter,
       panels,
-      isDAList,
-      currentListSize
+      articleClasses,
+      tagSelection,
+      currentListRange
     }
   },
   computed: {
-    visiblePanels() {
-      return this.panels.slice(0, this.currentListSize)
+    panelsInRange() {
+      return this.panels.filter(p => p.show).slice(0, this.currentListRange)
     },
-    articleClasses() {
-      const classes = []
-      if(this.isDAList) {
-        classes.push('xlarge')
-      }
-      return classes
+    panelsToShow() {
+      return this.panels.filter(p => p.show)
+    },
+    filterIsActive() {
+      return Object.values(this.tagSelection).reduce((a, v) => (+a) + (+v)) > 0
     }
   },
   methods: {
     optimizeTracking,
     getLocalizedDoc(article) {
       return localizeArticle(article, this.$i18n.locale)
+    },
+    toggleTag(tag) {
+      Object.keys(this.tagSelection).forEach(t => { this.tagSelection[t] = t === tag ? !this.tagSelection[t] : false })
+      if(this.filterIsActive) {
+        this.togglePanelVisibility()
+      } else {
+        this.resetFilter()
+      }
+    },
+    togglePanelVisibility() {
+      for(const panel of this.panels) {
+        if(panel.type === 'article') {
+          const article = panel.data
+          const tags = article.tags ? article.tags.split(' ').filter(t => t.trim().length > 0) : []
+          const visible = tags.reduce((acc, tag) => acc || this.tagSelection[tag], false)
+          panel.show = visible
+        }
+      }
+      this.currentListRange = this.firstPageSize
+    },
+    resetFilter() {
+      Object.keys(this.tagSelection).forEach(t => { this.tagSelection[t] = false })
+      for(const panel of this.panels) {
+        panel.show = true
+      }
     }
   }
 }
@@ -137,6 +179,15 @@ export default {
 @import '~assets/styles/resources';
 
 .article-list {
+  .filter {
+    margin: 0.5rem 1.5rem 0;
+    .button {
+      margin-right: 0.25rem;
+      &.active {
+        background-color: var(--iorg-paper);
+      }
+    }
+  }
   .article {
     > .detail {
       > h3 {
